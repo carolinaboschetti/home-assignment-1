d96b9dd85a2cdcaaafbbfbf21bf39b5b90ec62a4
Carolina Boschetti <30893634+carolinaboschetti@users.noreply.github.com>
2019-10-26 15:01:43 +0200

    :construction: Adding header and footer files

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+        <router-link to='/'><img src="../../Assets/cupcakesandcookies.png" alt="Cupcakes and Cookies logo"></router-link>
+        <div class="menu">
+            <router-link class="link-icon" to="/"><img src="../../Assets/house.png" alt="House"></router-link>
+            <router-link class="link-icon" to="/"><img src="../../Assets/compass-arrow.png" alt="Compass"></router-link>
+            <router-link class="link-icon" to="/"><img src="../../Assets/file.png" alt="File"></router-link>
+            <router-link class="link-icon" to="/"><img src="../../Assets/question-mark.png" alt="Question-mark"></router-link>
+            <router-link class="link-icon" to="/"><img src="../../Assets/person.png" alt="Person"></router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'TheHeader'
+}
+</script>
+
+<style>
+
+</style>
