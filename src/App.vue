942a9f58007e5e109d22a638dea4985487da3f96
Carolina Boschetti <30893634+carolinaboschetti@users.noreply.github.com>
2019-10-26 15:28:59 +0200

    :construction: Placing things correctly

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,14 @@ export default {
 }
 </script>
 <style lang="scss">
+body {
+    background: red url(assets/background.jpg) 0 0 repeat;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
-        <router-link to='/'><img src="../../Assets/cupcakesandcookies.png" alt="Cupcakes and Cookies logo"></router-link>
+        <router-link to='/'><img src="../assets/cupcakesandcookies.png" alt="Cupcakes and Cookies logo"></router-link>
         <div class="menu">
-            <router-link class="link-icon" to="/"><img src="../../Assets/house.png" alt="House"></router-link>
-            <router-link class="link-icon" to="/"><img src="../../Assets/compass-arrow.png" alt="Compass"></router-link>
-            <router-link class="link-icon" to="/"><img src="../../Assets/file.png" alt="File"></router-link>
-            <router-link class="link-icon" to="/"><img src="../../Assets/question-mark.png" alt="Question-mark"></router-link>
-            <router-link class="link-icon" to="/"><img src="../../Assets/person.png" alt="Person"></router-link>
+            <router-link class="link-icon" to="/"><img src="../assets/house.png" alt="House"></router-link>
+            <router-link class="link-icon" to="/"><img src="../assets/compass-arrow.png" alt="Compass"></router-link>
+            <router-link class="link-icon" to="/"><img src="../assets/file.png" alt="File"></router-link>
+            <router-link class="link-icon" to="/"><img src="../assets/question-mark.png" alt="Question-mark"></router-link>
+            <router-link class="link-icon" to="/"><img src="../assets/person.png" alt="Person"></router-link>
         </div>
     </header>
 </template>
@@ -18,5 +18,20 @@ export default {
 </script>
 
 <style>
-
+header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+.link-icon{
+    background-color: #ee2961;
+    border-radius: 50%;
+    padding: 8px;
+    min-width: 35px;
+    min-height: 35px;
+    display: inline-flex;
+    justify-content: center;
+    margin: 0 3px;
+}
 </style>
