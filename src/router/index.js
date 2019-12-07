44fd4874796793fd5b525667f8fa4cda774f16d8
Juliay <juliay@kth.se>
2019-12-07 16:55:28 +0100

    add api files

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Recipes from "../views/Recipes.vue";
 import About from "../views/About.vue";
+import Search from "../views/Search.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/about",
     name: "about",
     component: About
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: Search
   }
 ];
 
